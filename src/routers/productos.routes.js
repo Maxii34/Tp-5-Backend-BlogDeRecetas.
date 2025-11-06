79a1d0cf02468816d76e5da157bc93ac78b6a7ef
Maxi Ordoñez <exemaxi@outlook.com>
2025-11-06 14:32:23 -0300

    Se agrego la validaciones para crear una receta.

--- src/routers/productos.routes.js
+++ src/routers/productos.routes.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { crearRecetas, listarRecetas, borrarRecetas, editarRecetas, obtenerRecetas } from "../controllers/producto.controllers.js";
+import validarReceta from "../middlewares/validacionProductos.js";
 
 
 /*Get, Post, Path, Put, Delete*/
 //Router() se guarda en router.
 const router = Router();
 
-router.route('/').post(crearRecetas).get(listarRecetas);
+router.route('/').post(validarReceta,crearRecetas).get(listarRecetas);
 router.route('/:id').delete(borrarRecetas).put(editarRecetas).get(obtenerRecetas);
 
 
